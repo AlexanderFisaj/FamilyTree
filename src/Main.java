848af97d28d2c211f97cc5ba9baf44604638f3c9
FisayAA <fisay@fisay.ru>
2023-07-30 15:22:56 +0300

    hw 1 + hw 2: create project, write/read data in/on file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import family_tree.FamilyTree;
+import human.Gender;
+import human.Human;
+import writer.FileHandler;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String filePath = "src/family_tree/tree.out";
+
+        FileHandler fileHandler = new FileHandler();
+        FamilyTree tree = testTree();
+        System.out.println(tree);
+        fileHandler.save((Serializable) tree, filePath);
+
+//        FileHandler fileHandler = new FileHandler();
+//        FamilyTree tree = (FamilyTree) fileHandler.read(filePath);
+//        System.out.println(tree);
+
     }
+    static FamilyTree testTree(){
+        FamilyTree tree = new FamilyTree();
+
+        Human fedor = new Human("Федор", Gender.Male, LocalDate.of(1966,2,2));
+        Human galina = new Human("Галина", Gender.Female, LocalDate.of(1970,3,3));
+        tree.add(fedor);
+        tree.add(galina);
+        tree.setWedding(fedor.getId(), galina.getId());
+
+        Human elena = new Human("Елена", Gender.Female, LocalDate.of(1999, 3, 7),
+                fedor, galina);
+        Human andrey = new Human("Андрей", Gender.Male, LocalDate.of(2003,9,24),
+                fedor, galina);
+        tree.add(elena);
+        tree.add(andrey);
+
+        Human svetlana = new Human("Светлана", Gender.Female, LocalDate.of(1939,5,9));
+        svetlana.addChild(fedor);
+        tree.add(svetlana);
+
+        /* System.out.println(tree); */
+        return tree;
+    }
+
 }
